da929d9655fb0e149c57a12493d1780c9dbd974c
Sameer Zaher <sameerzaher@gmail.com>
2025-06-11 22:36:03 +0300

    Add static file serving for uploads directory
    - Integrated static file serving for the "uploads" directory using Express.
    - This allows access to uploaded files via the `/uploads` endpoint.

--- tech-stock-backend/index.js
+++ tech-stock-backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
+const path = require('path');
 
 const authRoutes = require('./routes/authroutes');
 const productRoutes = require('./routes/productroutes');
@@ -12,6 +13,9 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+
+// Serve static files from the "uploads" directory
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
